d9dc73eec0c703d1c3293c3b3766ee908dafb352
epg900 <epg900@gmail.com>
2024-12-31 13:46:48 +0330

    1.6

--- lamachat/lamachatapp/views.py
+++ lamachat/lamachatapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from backend import pdflrn,pdfres
+from lamachatapp.backend import pdflrn,pdfres
 # Create your views here.
 
 def index(request):
