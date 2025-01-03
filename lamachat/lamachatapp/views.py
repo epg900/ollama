f85b7594e304cc5d87e607daf875d0bc7abd9883
Ersci <47410365+epg900@users.noreply.github.com>
2025-01-03 13:42:52 +0330

    Update views.py

--- lamachat/lamachatapp/views.py
+++ lamachat/lamachatapp/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from lamachatapp.backend import pdflrn,pdfres
+#from lamachatapp.backend import pdflrn,pdfres
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
 
-#@csrf_exempt    
+@csrf_exempt    
 def res(request):
     msg = ""
     if request.method == "POST":
         msg = request.POST['msg']
-    response = pdfres(msg)
+    response = msg
+    #response = pdfres(msg)
     return HttpResponse(response)
 
 def pdf(request):
-    return pdflrn("/content/test.pdf")
+    return True #pdflrn("/content/test.pdf")
     
     
